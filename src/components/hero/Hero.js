3982d535290212345cc88f3e14f92af18727bf84
DIEGOHILLS <hillsdiego33@gmail.com>
2026-02-01 23:36:16 +0200

    mobile UI

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,60 +1,64 @@
-import './Hero.css';
-import Carousel from 'react-material-ui-carousel';
-import { Paper } from '@mui/material';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
-import {Link, useNavigate} from "react-router-dom";
-import Button from 'react-bootstrap/Button';
+import "./Hero.css";
+import Carousel from "react-material-ui-carousel";
+import { Paper } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
+const Hero = ({ movies }) => {
+  const navigate = useNavigate();
 
-const Hero = ({movies}) => {
-
-    const navigate = useNavigate();
-
-    function reviews(movieId)
-    {
-        navigate(`/Reviews/${movieId}`);
-    }
+  function reviews(movieId) {
+    navigate(`/Reviews/${movieId}`);
+  }
 
   return (
-    <div className ='movie-carousel-container'>
+    <div className="movie-carousel-container">
       <Carousel>
-        {
-            movies?.map((movie) =>{
-                return(
-                    <Paper key={movie.imdbId}>
-                        <div className = 'movie-card-container'>
-                            <div className="movie-card" style={{"--img": `url(${movie.backdrops[0]})`}}>
-                                <div className="movie-detail">
-                                    <div className="movie-poster">
-                                        <img src={movie.poster} alt="" />
-                                    </div>
-                                    <div className="movie-title">
-                                        <h4>{movie.title}</h4>
-                                    </div>
-                                    <div className="movie-buttons-container">
-                                        <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
-                                            <div className="play-button-icon-container">
-                                                <FontAwesomeIcon className="play-button-icon"
-                                                    icon = {faCirclePlay}
-                                                />
-                                            </div>
-                                        </Link>
+        {movies?.map((movie) => {
+          const backdrop = movie?.backdrops?.[0] || movie?.poster || "";
+          const trailerId = movie?.trailerLink
+            ? movie.trailerLink.substring(movie.trailerLink.length - 11)
+            : "";
 
-                                        <div className="movie-review-button-container">
-                                            <Button variant ="info" onClick={() => reviews(movie.imdbId)} >Reviews</Button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </Paper>
-                )
-            })
-        }
+          return (
+            <Paper key={movie.imdbId}>
+              <div className="movie-card-container">
+                <div className="movie-card" style={{ "--img": `url(${backdrop})` }}>
+                  <div className="movie-detail">
+                    <div className="movie-poster">
+                      <img src={movie.poster} alt={movie.title || ""} />
+                    </div>
+
+                    <div className="movie-title">
+                      <h4>{movie.title}</h4>
+                    </div>
+
+                    <div className="movie-buttons-container">
+                      {trailerId ? (
+                        <Link to={`/Trailer/${trailerId}`}>
+                          <div className="play-button-icon-container">
+                            <FontAwesomeIcon className="play-button-icon" icon={faCirclePlay} />
+                          </div>
+                        </Link>
+                      ) : null}
+
+                      <div className="movie-review-button-container">
+                        <Button variant="info" onClick={() => reviews(movie.imdbId)}>
+                          Reviews
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Paper>
+          );
+        })}
       </Carousel>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
